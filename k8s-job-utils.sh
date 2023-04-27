a96dc91f76e8ce2d4b8ae7e9d88a045b37370c7a
Ciprian <cursu@axetrading.com>
2023-04-27 14:32:20 +0300

    optimized the script

--- k8s-job-utils.sh
+++ k8s-job-utils.sh
@@ -1,46 +1,42 @@
-function assert_terraform_state_exists () {
-    local bucket
-    bucket="$1"
-    local key
-    key="$2"
-    if [ -z "$FIRST_RUN" ]; then
-        aws s3api head-object --bucket "$bucket" --key "$key" >/dev/null 2>&1 || not_exist=true
-        if [ $not_exist ]; then
-            echo "error: no Terraform state found at s3://$bucket/$key" >&2
-            echo "if this is the first run you should set the FIRST_RUN=1 environment variable" >&2
-            return 1
-        fi
-    fi
+function assert_terraform_state_exists() {
+    local bucket="$1"
+    local key="$2"
+    [ -z "$FIRST_RUN" ] || return 0
+    aws s3api head-object --bucket "$bucket" --key "$key" >/dev/null 2>&1 || {
+        echo "error: no Terraform state found at s3://$bucket/$key" >&2
+        echo "if this is the first run you should set the FIRST_RUN=1 environment variable" >&2
+        return 1
+    }
 }
 
-function initialise_terraform_workspace {
-    local tfstate_bucket; tfstate_bucket="$1"
-    local tflocks_table; tflocks_table="$2"
-    local name; name="$3"
-    local workspace_name; workspace_name="$4"
-    local key; key="terraform.tfstate"
+function initialise_terraform() {
+    local cmd=$1
+    local tfstate_bucket="$2"
+    local tflocks_table="$3"
+    local repo_name="$4"
+    local workspace_name="$5"
+    local key="$repo_name/${workspace_name:-dev}.tfstate"
 
-    assert_terraform_state_exists "$tfstate_bucket" "$name/$workspace_name/$key"
-    
-    terraform init -reconfigure \
-        -backend-config="bucket=$tfstate_bucket" \
-        -backend-config="dynamodb_table=$tflocks_table" \
-        -backend-config="workspace_key_prefix=$name" \
-        -backend-config="key=$key"
-    
-    terraform workspace select "$workspace_name" || terraform workspace new "$workspace_name"
-}
-
-function initialise_terraform_dev {
-    local tfstate_bucket; tfstate_bucket="$1"
-    local tflocks_table; tflocks_table="$2"
-    local name; name="$3"
-    local key; key="$name/dev.tfstate"
+    local tf_global_args=""
+    [ -z "$TF_CLI_CHDIR" ] || tf_global_args="-chdir=$TF_CLI_CHDIR"
 
     assert_terraform_state_exists "$tfstate_bucket" "$key"
-    
-    terraform init -reconfigure \
+
+    terraform $tf_global_args init -reconfigure \
         -backend-config="bucket=$tfstate_bucket" \
         -backend-config="dynamodb_table=$tflocks_table" \
-        -backend-config="key=$key"
+        -backend-config="key=$key" \
+        $([ "$cmd" = "workspace" ] && echo "-backend-config=\"workspace_key_prefix=$repo_name\"")
+
+    [ "$cmd" = "workspace" ] && {
+        terraform $tf_global_args workspace select "$workspace_name" || terraform $tf_global_args workspace new "$workspace_name"
+    }
+}
+
+function initialise_terraform_workspace() {
+    initialise_terraform "workspace" "$@"
+}
+
+function initialise_terraform_dev() {
+    initialise_terraform "dev" "$@"
 }
